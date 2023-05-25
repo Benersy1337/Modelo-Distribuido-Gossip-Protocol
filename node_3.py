4a7d60a9e5564d11acc6351673d25e769c3b5357
Benersy1337 <benersy1337@gmail.com>
2023-05-25 11:04:56 -0300

    modelo distribuido

---
+++ node_3.py
@@ -0,0 +1,33 @@
+import socket
+import threading
+
+def receive_messages(sock):
+    while True:
+        data, addr = sock.recvfrom(1024)
+        print(f'Received from Node:{addr[1]}', data.decode('utf-8'))
+
+def send_message(sock, host, send_ports):
+    while True:
+        message = input('Enter message: ')
+        for port in send_ports:
+            sock.sendto(message.encode('utf-8'), (host, port))
+
+def main():
+    host = 'localhost'
+    port = 8003
+
+    send_ports = [8001, 8002, 8004, 8005, 8006]
+    
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((host, port))
+
+    receive_thread = threading.Thread(target=receive_messages, args=(sock,))
+    receive_thread.start()
+
+    send_thread = threading.Thread(target=send_message, args=(sock, host, send_ports))
+    send_thread.start()
+    
+    send_thread.join()
+
+if __name__ == '__main__':
+    main()
